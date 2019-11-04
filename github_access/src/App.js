85c21f4e8bd7b90e4c784ef80fd39408f2ce8101
dillonryan98 <dillonryan98@gmail.com>
2019-11-04 22:37:02 +0000

    Setting up landing page  basics

--- github_access/src/App.js
+++ github_access/src/App.js
@@ -2,25 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    return (
+      <div>
+      <center>
+      <h1>Welcome to this page</h1>
+      </center>
+      </div>
+    );
+
 }
 
-export default App;
+function navbar() {
+  return (
+    <h1>Github Access </h1>
+  );
+}
