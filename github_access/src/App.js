db11e6ceaf5694406461c607a9878400770ca4fa
dillonryan98 <dillonryan98@gmail.com>
2019-11-07 19:57:37 +0000

    Adding a top bar and making it possible to create a login

--- github_access/src/App.js
+++ github_access/src/App.js
@@ -1,21 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import LogIn from "./LogIn"
+import 'bootstrap/dist/css/bootstrap.css' ;
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default function App() {
-    return (
+  return (
+    <Router>
       <div>
-      <center>
-      <h1>Welcome to this page</h1>
-      </center>
+        <Navbar />
+        <div className="space"></div>
+        <Switch>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <Route path="/Home">
+            <Home />
+          </Route>
+          <Route path="/">
+            <LogIn /> // - this sets the default page to be the login page
+          </Route>
+        </Switch>
       </div>
-    );
+    </Router>
+  );
+}
+
+function Login(){
+  return(
+    <div>
+      <LogIn />
+    </div>
+  );
 
 }
 
-function navbar() {
+function Navbar(){
   return (
-    <h1>Github Access </h1>
+    <nav className="navbar navbar-expand-md navbar-dark bg-dark sticky-top">
+      <button type="button" className="navbar-toggler text-cente" data-toggle="collapse" data-target="#collapse_target">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="collapse_target">
+        <ul className="navbar-nav">
+          <li className ="nav-item">
+            <center>
+            <a className="nav-link" href="/">Github API Visualisation Log in</a>
+            </center>
+          </li>
+          <li className ="nav-item">
+            <a className="nav-link" href="/Home">Home</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+
+}
+
+function Home() {
+  return (
+      <div>
+        <UserProfile name="Dillon" />
+
+    </div>
+  );
+}
+
+
+function UserProfile(Username){
+  return(
+    <div className="container">
+      <h2 className="text-center">Welcome Back {Username.name} !</h2>
+    </div>
   );
 }
