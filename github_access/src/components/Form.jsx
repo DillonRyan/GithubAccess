0276f7d726bbd7dce15c586595f93576b7196652
dillonryan98 <dillonryan98@gmail.com>
2019-11-14 11:45:38 +0000

    Adding in components which in turn will remove the login page and will leave it all on one page for now

---
+++ github_access/src/components/Form.jsx
@@ -0,0 +1,18 @@
+import React from 'react';const Form = (props) => {
+  return (<form onSubmit={(event) => props.handleUserFormSubmit(event)}>
+      <label>
+        <input name="username"
+        type="text"
+        placeholder="username"
+        required
+        value={props.formData.username}
+        onChange={props.handleFormChange}
+      />
+      </label>
+      <div>
+      <input
+        type="submit"
+        value="Submit"
+      />
+    </div>
+    </form>)};export default Form;
