c7d75857db184ab0f7cae5bc49138b23602f33ac
dillonryan98 <dillonryan98@gmail.com>
2019-11-19 11:37:29 +0000

    Getting the bar chart working, still more work to get it working

--- github_access/src/components/LanguageList.jsx
+++ github_access/src/components/LanguageList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from "react";
 import {
   LineChart,
   CartesianGrid,
@@ -14,13 +14,31 @@ import {
   BarChart,
   ResponsiveContainer
 } from "recharts";
+
 const LanguageList = (props) => {
   if (props.langslist) {
     return (
         <ul>
           {Object.entries(props.langslist).map(([key,value]) =>
             <li key={key}>
-              
+            <BarChart
+               width={700}
+               height={350}
+ 		          data={key, value}
+ 		          margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
+ 		        >
+ 		          <CartesianGrid strokeDasharray="3 3" fill="white" />
+ 		          <XAxis dataKey="key" />
+ 		          <YAxis dateKey="value" tick={false}/>
+ 		          <Tooltip />
+ 		          <Legend />
+ 		          <Bar dataKey="value" fill="#8884d8" />
+ 		          <ReferenceLine y={0} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Normal', fill: 'black', fontSize: 18}} stroke="green"/>
+ 		          <ReferenceLine y={7} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Mild', fill: 'black', fontSize: 18}} stroke="blue"/>
+ 		          <ReferenceLine y={9} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Moderate', fill: 'black', fontSize: 18}} stroke="black"/>
+ 		          <ReferenceLine y={14} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Severe', fill: 'black', fontSize: 18}} stroke="purple"/>
+ 		          <ReferenceLine y={19} label={{ infront: 'true', position: 'insideBottomRight', value: 'Extremely Severe', fill: 'black', fontSize: 18}} stroke="red"/>
+ 		       </BarChart>
               {key} - {value}
             </li>
           )}
