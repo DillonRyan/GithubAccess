0d6a3e9b148a7b749a50bfc615bf403223a9afa1
dillonryan98 <dillonryan98@gmail.com>
2019-11-19 21:40:34 +0000

    Making slow progress on getting the bar chart to work

--- github_access/src/components/LanguageList.jsx
+++ github_access/src/components/LanguageList.jsx
@@ -21,26 +21,29 @@ const LanguageList = (props) => {
         <ul>
           {Object.entries(props.langslist).map(([key,value]) =>
             <li key={key}>
-            <BarChart
-               width={700}
-               height={350}
- 		          data={key, value}
- 		          margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
- 		        >
- 		          <CartesianGrid strokeDasharray="3 3" fill="white" />
- 		          <XAxis dataKey="key" />
- 		          <YAxis dateKey="value" tick={false}/>
- 		          <Tooltip />
- 		          <Legend />
- 		          <Bar dataKey="value" fill="#8884d8" />
- 		          <ReferenceLine y={0} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Normal', fill: 'black', fontSize: 18}} stroke="green"/>
- 		          <ReferenceLine y={7} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Mild', fill: 'black', fontSize: 18}} stroke="blue"/>
- 		          <ReferenceLine y={9} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Moderate', fill: 'black', fontSize: 18}} stroke="black"/>
- 		          <ReferenceLine y={14} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Severe', fill: 'black', fontSize: 18}} stroke="purple"/>
- 		          <ReferenceLine y={19} label={{ infront: 'true', position: 'insideBottomRight', value: 'Extremely Severe', fill: 'black', fontSize: 18}} stroke="red"/>
- 		       </BarChart>
               {key} - {value}
+              <BarChart
+                 width={700}
+                 height={350}
+                data={{key}, {value}}
+                margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
+              >
+                <CartesianGrid strokeDasharray="3 3" fill="white" />
+                <XAxis dataKey="key" />
+                <YAxis dateKey="value" tick={false}/>
+                <Tooltip />
+                <Legend />
+                <Bar dataKey={key} fill="#8884d8" />
+                <ReferenceLine y={0} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Normal', fill: 'black', fontSize: 18}} stroke="green"/>
+                <ReferenceLine y={7} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Mild', fill: 'black', fontSize: 18}} stroke="blue"/>
+                <ReferenceLine y={9} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Moderate', fill: 'black', fontSize: 18}} stroke="black"/>
+                <ReferenceLine y={14} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Severe', fill: 'black', fontSize: 18}} stroke="purple"/>
+                <ReferenceLine y={19} label={{ infront: 'true', position: 'insideBottomRight', value: 'Extremely Severe', fill: 'black', fontSize: 18}} stroke="red"/>
+             </BarChart>
+
             </li>
+
+
           )}
         </ul>
       )
