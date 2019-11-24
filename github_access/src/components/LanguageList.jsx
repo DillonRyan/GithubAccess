caed267f414decff3b2f08d445b670fa0751ab6d
dillonryan98 <dillonryan98@gmail.com>
2019-11-24 18:09:55 +0000

    Graph still not working

--- github_access/src/components/LanguageList.jsx
+++ github_access/src/components/LanguageList.jsx
@@ -51,6 +51,26 @@ const LanguageList = (props) => {
     {
       var totalcount =  Object.entries(props.langslist).map(([key,value]) =>
       (value.count)).reduce((pv, cv) => pv+cv, 0 ) }
+      return(
+      <BarChart
+         width={700}
+         height={350}
+         data={data}
+         margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
+       >
+         <CartesianGrid strokeDasharray="3 3" fill="white" />
+         <XAxis dataKey="date" />
+         <YAxis dateKey="score" tick={false}/>
+         <Tooltip />
+         <Legend />
+         <Bar dataKey="score" fill="#8884d8" />
+         <ReferenceLine y={0} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Zero', fill: 'black', fontSize: 18}} stroke="green"/>
+         <ReferenceLine y={5} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Fiv', fill: 'black', fontSize: 18}} stroke="blue"/>
+         <ReferenceLine y={10} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Ten', fill: 'black', fontSize: 18}} stroke="black"/>
+         <ReferenceLine y={15} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Fifteen', fill: 'black', fontSize: 18}} stroke="purple"/>
+         <ReferenceLine y={20} label={{ infront: 'true', position: 'insideBottomRight', value: 'Twenty', fill: 'black', fontSize: 18}} stroke="red"/>
+      </BarChart>
+    )
         return(
           <ul>
             {Object.entries(props.langslist).map(([key,value]) =>
@@ -58,24 +78,8 @@ const LanguageList = (props) => {
                 {key} - {value}
                 </li>
               )}
-          <BarChart
-             width={700}
-             height={350}
-             data={data}
-             margin={{ top: 5, right: 30, left: 5, bottom: 5 }}
-           >
-             <CartesianGrid strokeDasharray="3 3" fill="white" />
-             <XAxis dataKey="date" />
-             <YAxis dateKey="score" tick={false}/>
-             <Tooltip />
-             <Legend />
-             <Bar dataKey="score" fill="#8884d8" />
-             <ReferenceLine y={0} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Zero', fill: 'black', fontSize: 18}} stroke="green"/>
-             <ReferenceLine y={5} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Fiv', fill: 'black', fontSize: 18}} stroke="blue"/>
-             <ReferenceLine y={10} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Ten', fill: 'black', fontSize: 18}} stroke="black"/>
-             <ReferenceLine y={15} label={{ infront: 'true', position: 'insideBottomRight',  value: 'Fifteen', fill: 'black', fontSize: 18}} stroke="purple"/>
-             <ReferenceLine y={20} label={{ infront: 'true', position: 'insideBottomRight', value: 'Twenty', fill: 'black', fontSize: 18}} stroke="red"/>
-          </BarChart>
+
+
             </ul>
         )
 }
